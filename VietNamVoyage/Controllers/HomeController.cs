e99261c0e110b6ec6582a56124de664dca64b976
Lu1702 <128778245+Lu1702@users.noreply.github.com>
2026-07-01 21:43:49 +0700

    Init_Layout_Home_Index

--- VietNamVoyage/Controllers/HomeController.cs
+++ VietNamVoyage/Controllers/HomeController.cs
@@ -8,14 +8,6 @@ namespace VietNamVoyage.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Test()
-        {
-            return View();
-        }
-        public ActionResult Home()
-        {
-            return View();
-        }
         public ActionResult Index()
         {
             return View();
