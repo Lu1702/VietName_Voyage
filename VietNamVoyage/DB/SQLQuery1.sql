3a106a270570733dbc50516e6732079ae0dbd607
Lu1702 <128778245+Lu1702@users.noreply.github.com>
2026-07-15 10:05:51 +0700

    adding_db_scripts_sql

---
+++ VietNamVoyage/DB/SQLQuery1.sql
@@ -0,0 +1,45 @@
+﻿CREATE DATABASE TravelDB;
+GO
+
+USE TravelDB;
+GO
+
+CREATE TABLE Admin
+(
+    IdUser INT IDENTITY(1,1) PRIMARY KEY,
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    Password NVARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Users
+(
+    IdUser INT IDENTITY(1,1) PRIMARY KEY,
+    Username NVARCHAR(50) NOT NULL UNIQUE,
+    Password NVARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Destinations
+(
+    IdDestination INT IDENTITY(1,1) PRIMARY KEY,
+    NameDestination NVARCHAR(100) NOT NULL,
+    Image NVARCHAR(500),
+    Description NVARCHAR(MAX)
+);
+
+CREATE TABLE Reviews
+(
+    IdReview INT IDENTITY(1,1) PRIMARY KEY,
+
+    IdUser INT NOT NULL,
+    IdDestination INT NOT NULL,
+
+    Review NVARCHAR(MAX) NOT NULL,
+
+    CONSTRAINT FK_Reviews_Users
+        FOREIGN KEY (IdUser)
+        REFERENCES Users(IdUser),
+
+    CONSTRAINT FK_Reviews_Destinations
+        FOREIGN KEY (IdDestination)
+        REFERENCES Destinations(IdDestination)
+);
